d8680accf3c22a22eb6fb2f007a37f3e8bfefe16
Nathan Howell <nathan.d.howell@gmail.com>
2020-07-16 15:41:56 -0700

    Add a stub

---
+++ src/main/kotlin/com/godaddy/icfp2020/Application.kt
@@ -0,0 +1,8 @@
+package com.godaddy.icfp2020
+
+object Application {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello world!")
+    }
+}
---
+++ src/test/kotlin/com/godaddy/icfp2020/ApplicationTest.kt
@@ -0,0 +1,13 @@
+package com.godaddy.icfp2020
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ApplicationTest {
+
+    @Test
+    fun main() {
+        Application.main(arrayOf())
+    }
+}
