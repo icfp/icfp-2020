688daef5d2e8e96bbed2bfbad7135922e79de4e6
Jake Swenson <jake@jakeswenson.com>
2020-07-17 20:58:44 -0700

    connecting parser to eval, added top level run method

--- src/tests.rs
+++ src/tests.rs
@@ -7,3 +7,14 @@ fn run_inc_1() {
 
     assert_eq!(symbol, Lit(2))
 }
+
+#[test]
+fn run_inc_var() {
+    let symbol = super::run(
+        ":1096 = ap inc 1
+         :302 = ap inc :1096",
+    );
+    dbg!(&symbol);
+
+    assert_eq!(symbol, Lit(3))
+}
